af54428d4ee9fb00157a5adf736b35e399dd501e
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-04-17 04:17:17 +0700

    Update crud.go

--- crud/crud.go
+++ crud/crud.go
@@ -5,7 +5,7 @@ import (
 	"github.com/cockroachdb/pebble"
 )
 
-// CreateKeyValue adds a new key-value pair to the database.
+// adds a new key-value pair to the database.
 func CreateKeyValue(db *pebble.DB, key, value []byte) error {
 	err := db.Set(key, value, nil)
 	if err != nil {
@@ -14,7 +14,7 @@ func CreateKeyValue(db *pebble.DB, key, value []byte) error {
 	return nil
 }
 
-// ReadKeyValue retrieves the value associated with a key.
+// retrieves the value associated with a key.
 func ReadKeyValue(db *pebble.DB, key []byte) ([]byte, error) {
 	retrievedValue, closer, err := db.Get(key)
 	if err != nil {
@@ -24,7 +24,7 @@ func ReadKeyValue(db *pebble.DB, key []byte) ([]byte, error) {
 	return retrievedValue, nil
 }
 
-// UpdateKeyValue updates an existing key-value pair with a new value.
+// updates an existing key-value pair with a new value.
 func UpdateKeyValue(db *pebble.DB, key, newValue []byte) error {
 	err := db.Set(key, newValue, nil)
 	if err != nil {
@@ -33,7 +33,7 @@ func UpdateKeyValue(db *pebble.DB, key, newValue []byte) error {
 	return nil
 }
 
-// DeleteKeyValue removes a key-value pair from the database.
+// removes a key-value pair from the database.
 func DeleteKeyValue(db *pebble.DB, key []byte) error {
 	err := db.Delete(key, nil)
 	if err != nil {
