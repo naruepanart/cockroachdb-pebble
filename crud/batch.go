7e802bb34cb555f0d0a8fe2df329060c83a130f4
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-04-17 18:45:26 +0700

    Update batch.go

--- crud/batch.go
+++ crud/batch.go
@@ -1,21 +1,11 @@
 package crud
 
 import (
-	"fmt"
 	"github.com/cockroachdb/pebble"
 )
 
 func BatchCreateKeyValue(batch *pebble.Batch, key, value []byte) error {
 	return batch.Set(key, value, pebble.NoSync)
-}
-
-func BatchReadKeyValue(db *pebble.DB, key []byte) ([]byte, error) {
-	value, closer, err := db.Get(key)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get value for key: %w", err)
-	}
-	defer closer.Close()
-	return value, nil
 }
 
 func BatchUpdateKeyValue(batch *pebble.Batch, key, value []byte) error {
