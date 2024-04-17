8ecd3f7158ebbc285f2c5449d14bd64e40db7a8d
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-04-17 18:22:40 +0700

    Update batch.go

--- crud/batch.go
+++ crud/batch.go
@@ -5,17 +5,10 @@ import (
 	"github.com/cockroachdb/pebble"
 )
 
-// adds a new key-value pair to the database using a batch operation for improved performance.
-func BatchCreateKeyValue(db *pebble.DB, key, value []byte) error {
-	batch := db.NewBatch()
-	defer batch.Close()
-
-	batch.Set(key, value, pebble.NoSync)
-	batch.Commit(pebble.Sync)
-	return nil
+func BatchCreateKeyValue(batch *pebble.Batch, key, value []byte) error {
+	return batch.Set(key, value, pebble.NoSync)
 }
 
-// retrieves the value associated with a key and properly handles the closer.
 func BatchReadKeyValue(db *pebble.DB, key []byte) ([]byte, error) {
 	value, closer, err := db.Get(key)
 	if err != nil {
@@ -25,22 +18,10 @@ func BatchReadKeyValue(db *pebble.DB, key []byte) ([]byte, error) {
 	return value, nil
 }
 
-// updates an existing key-value pair with a new value using a batch operation for improved performance.
-func BatchUpdateKeyValue(db *pebble.DB, key, newValue []byte) error {
-	batch := db.NewBatch()
-	defer batch.Close()
-
-	batch.Set(key, newValue, pebble.NoSync)
-	batch.Commit(pebble.Sync)
-	return nil
+func BatchUpdateKeyValue(batch *pebble.Batch, key, value []byte) error {
+	return batch.Set(key, value, pebble.NoSync)
 }
 
-// removes a key-value pair from the database using a batch operation for improved performance.
-func BatchDeleteKeyValue(db *pebble.DB, key []byte) error {
-	batch := db.NewBatch()
-	defer batch.Close()
-
-	batch.Delete(key, pebble.NoSync)
-	batch.Commit(pebble.Sync)
-	return nil
+func BatchDeleteKeyValue(batch *pebble.Batch, key []byte) error {
+	return batch.Delete(key, pebble.NoSync)
 }
