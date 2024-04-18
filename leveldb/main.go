d9c7fd60b90303761716c63117733f32d44cb348
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-04-18 21:19:38 +0700

    Update main.go

--- leveldb/main.go
+++ leveldb/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/syndtr/goleveldb/leveldb"
 	"log"
 )
 
 const dbPath = "../abc-pebble-db"
 
 func main() {
-	db, err := openDatabase(dbPath)
+	db, err := OpenDatabase(dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,72 +17,24 @@ func main() {
 	key := []byte("key1")
 
 	// Perform create, read, update, delete operations
-	err = createKeyValue(db, key, []byte("value1"))
+	err = Create(db, key, []byte("value1"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = readKeyValue(db, key)
+	v, err := FindOne(db, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(string(v))
+
+	err = Update(db, key, []byte("newValue1"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = updateKeyValue(db, key, []byte("newValue1"))
+	err = Remove(db, key)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	err = deleteKeyValue(db, key)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// openDatabase encapsulates the database opening and error handling
-func openDatabase(path string) (*leveldb.DB, error) {
-	db, err := leveldb.OpenFile(path, nil)
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
-}
-
-// createKeyValue encapsulates creating a key-value pair in the database
-func createKeyValue(db *leveldb.DB, key, value []byte) error {
-	err := db.Put(key, value, nil)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Key-value pair created")
-	return nil
-}
-
-// readKeyValue encapsulates reading a key-value pair from the database
-func readKeyValue(db *leveldb.DB, key []byte) error {
-	value, err := db.Get(key, nil)
-	if err != nil {
-		return err
-	}
-	fmt.Println(string(key), string(value))
-	return nil
-}
-
-// updateKeyValue encapsulates updating a key-value pair in the database
-func updateKeyValue(db *leveldb.DB, key, value []byte) error {
-	err := db.Put(key, value, nil)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Key-value pair updated")
-	return nil
-}
-
-// deleteKeyValue encapsulates deleting a key-value pair from the database
-func deleteKeyValue(db *leveldb.DB, key []byte) error {
-	err := db.Delete(key, nil)
-	if err != nil {
-		return err
-	}
-	fmt.Println("Key-value pair deleted")
-	return nil
 }
