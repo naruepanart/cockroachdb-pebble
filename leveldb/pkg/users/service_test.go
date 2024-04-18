9ec525a4db57ca79eeba55c1ce8643237e02a000
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-04-18 23:15:55 +0700

    Update service_test.go

--- leveldb/pkg/users/service_test.go
+++ leveldb/pkg/users/service_test.go
@@ -37,3 +37,18 @@ func BenchmarkFindOne(b *testing.B) {
 		FindOne(db, key)
 	}
 }
+
+func BenchmarkRemove(b *testing.B) {
+	db, err := database.ConnLevelDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Reset timer and run the benchmark
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		key := []byte(fmt.Sprintf("benchmarkKey_%d", i))
+		Remove(db, key)
+	}
+}
